c10daaa0810be62398b43b747e1253e8789ffb7d
Gavin Kirkreit <gkirkreit@mail.csuchico.edu>
2016-10-10 17:21:45 -0700

    added the has success line

--- form-validation.js
+++ form-validation.js
@@ -3,13 +3,18 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
+if (userEntered.length < 6){
   //Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
+}
 
+else{
+  document.getElementById("usernameGroup").classList.add("has-success");
+}
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
